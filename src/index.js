fb5f610d4ad6782bc96d2e224baf61d58dffd92c
Siverena <vpupkin@company.com>
2022-05-29 17:59:07 +0300

    lesson 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
-const App = () => {
-    return <h1>hello</h1>
-}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
