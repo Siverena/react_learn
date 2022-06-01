67889b166015f027151d13d80c263e2e1042372c
Siverena <vpupkin@company.com>
2022-06-01 22:17:20 +0300

    create webpack config, add eslint, add husky

--- src/components/classes/Message.js
+++ src/components/classes/Message.js
@@ -1,21 +1,19 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
-
-
-export class Message extends Component{
-    state={
-        msg: 'msg'
-    };
-    handleClick = ()=>{
-    this.setState({ msg: 'this.state.msg'})
-    }
-    render(){
-        return(
-            <>
-            <div>{this.state.msg}</div>
-            <div>{this.props.msg}</div>
-            <button onClick={this.handleClick}>click</button>
-            </>
-        )
-    }
+export class Message extends Component {
+  state = {
+    msg: 'msg',
+  };
+  handleClick = () => {
+    this.setState({ msg: 'this.state.msg' });
+  };
+  render() {
+    return (
+      <>
+        <div>{this.state.msg}</div>
+        <div>{this.props.msg}</div>
+        <button onClick={this.handleClick}>click</button>
+      </>
+    );
+  }
 }
